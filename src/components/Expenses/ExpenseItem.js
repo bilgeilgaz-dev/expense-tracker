5e40c41d932f6690ae411f4a84caa96f0d3a6748
Bilge Ilgaz <be@newbanking.com>
2022-10-19 17:43:13 +0100

    Added a new expense form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
 import './ExpenseItem.css';
 import './ExpenseDate.js'
 import ExpenseDate from './ExpenseDate.js';
+import Card from '../UI/Card.js';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitle = () => {
+    console.log('button clicked')
+    setTitle('Updated!');
+  }
+
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <div>
         <ExpenseDate date={props.date}/>
       </div>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+      <button onClick={changeTitle}>Change Title</button>
+    </Card>
   );
 }
 
